cad1910ef7ebb269c190aadf5f5493fd3c84d40e
tancs1 <m.tanveercs@gmail.com>
2024-09-10 11:11:34 +0500

    complete resume writing header

--- src/app/commonComponents/header/header.component.ts
+++ src/app/commonComponents/header/header.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isResumeWritingVisible = false;
 
+  showResumeWriting() {
+    this.isResumeWritingVisible = true;
+  }
+
+  keepResumeWriting() {
+    this.isResumeWritingVisible = true;
+  }
+
+  handleMouseLeave() {
+    // Delay hiding to allow user to move the cursor to the content
+    setTimeout(() => {
+      if (!document.querySelector('.resume-writing-container:hover') && !document.querySelector('#resumewrite:hover')) {
+        this.isResumeWritingVisible = false;
+      }
+    }, 100);
+  }
 }
