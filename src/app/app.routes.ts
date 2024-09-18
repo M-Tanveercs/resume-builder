e138382b65564aea69beae8cfbc19cf59e45bd4a
tancs1 <m.tanveercs@gmail.com>
2024-09-18 11:02:51 +0500

    start banner

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,17 @@ import { HomeComponent } from './commonComponents/home/home.component';
 import { MainComponent } from './commonComponents/main/main.component';
 
 export const routes: Routes = [
-    {path:'',redirectTo:'home'},
-    {path:'home',component:HomeComponent}
+    {path:'',component:MainComponent,
+
+        children:[
+         { path: '', 
+          redirectTo: 'home', // Redirect to the home 
+          pathMatch: 'full' 
+        },
+        {
+            path:'home',
+            component:HomeComponent
+        }
+    
+    ]}
 ];
