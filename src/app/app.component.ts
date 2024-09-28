2f212a76fd2f3f9a86f5128af10062c189990f79
Muhammad Tanveer <m.tanveercs@gmail.com>
2024-09-28 10:39:33 +0500

    chnages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './commonComponents/home/home.component';
+
 import { MainComponent } from "./commonComponents/main/main.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, MainComponent],
+  imports: [RouterOutlet, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/commonComponents/main/main.component.ts
+++ src/app/commonComponents/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent,HomeComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
